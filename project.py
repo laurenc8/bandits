2ed7b8bafd1e24c0af139718c77bafff96c5e2b9
LAUREN <lychen@college.harvard.edu>
2023-11-14 22:17:28 -0500

    our algorithm, not doing better than UCB

--- project.py
+++ project.py
@@ -10,10 +10,14 @@ class Bandit:
         self,
         k_arm=10,
         initial=0.0,
+        algo='UCB',
+        p_mean=0.5,
     ):
         self.k = k_arm
         self.time = 0
         self.initial = initial
+        self.algo = algo
+        self.p_mean = p_mean
 
     def reset(self):
         # we'll use self.q_true to track the real reward for each action
@@ -28,23 +32,48 @@ class Bandit:
 
         self.time = 0
 
-    # get an action for this bandit using UCB
+    # get an action for this bandit
+    # returns a list of tuples (arm, proportion) where proportions sum to 1
     def act(self):
-        q_best = np.argmax(self.q_estimation + np.sqrt(4 / 3 * np.log(self.time) / self.action_count))
+        UCB_arm = np.argmax(self.q_estimation + np.sqrt(4 * np.log(self.time) / self.action_count))
+        best_mean_arm = np.random.choice(np.where(self.q_estimation == np.max(self.q_estimation))[0])
+        if self.algo == 'UCB':
+            q_best = [(UCB_arm, 1)]
+        elif self.algo == 'Ours':
+            if best_mean_arm == UCB_arm:
+                q_best = [(UCB_arm, 1)]
+            else:
+                q_best = [(best_mean_arm, self.p_mean), (UCB_arm, 1 - self.p_mean)]
 
         return q_best
 
     # take an action, update estimation for this action
     def step(self, action):
-        # generate the reward under N(real reward, 1)
-        reward = np.random.uniform(low = -1, high = 1) + self.q_true[action]
+        reward = 0
+        for arm, p in action:
+            # generate the reward under Unif(real_reward - 1, real_reward + 1)
+            # reward += (np.random.uniform(low = -1, high = 1) + self.q_true[arm]) * p
+            # generate the reward under N(real_reward, 1)
+            reward += (np.random.normal(self.q_true[arm], 1)) * p
+            self.action_count[arm] += p
+        
         self.time += 1
-        self.action_count[action] += 1
+        
+        # calculate c
+        numerator = reward
+        denominator = 0
+        if self.algo == 'Ours':
+            for arm, p in action:
+                numerator -= p * self.q_estimation[arm]
+                denominator += p ** 2
+            c = numerator/denominator
 
-        # update estimation using sample averages
-        self.q_estimation[action] += (
-            reward - self.q_estimation[action]
-        ) / self.action_count[action]
+        for arm, p in action:
+            # update estimation using sample averages
+            if self.algo == 'UCB' or len(action) == 1:
+                self.q_estimation[arm] += (reward - self.q_estimation[arm]) / self.action_count[arm]
+            elif self.algo == 'Ours':
+                self.q_estimation[arm] += p * p * c / self.action_count[arm]
         
         return reward
 
@@ -55,7 +84,7 @@ def simulate(runs, time, bandits):
         for r in trange(runs):
             bandit.reset()
             for j in range(bandit.k):
-                action = j
+                action = [(j, 1)]
                 reward = bandit.step(action)
                 rewards[i, r, j] = reward
             for t in range(bandit.k, time):
@@ -66,16 +95,18 @@ def simulate(runs, time, bandits):
     return mean_rewards
 
 def project(runs=2000, time=1000):
-    bandits = [Bandit()]
+    bandits = [Bandit(), Bandit(algo='Ours')]
+    labels = ["UCB", "Ours"]
     rewards = simulate(runs, time, bandits)
     plt.figure(figsize=(20, 8))
-    for reward in rewards:
-        plt.plot(reward)
+    for (label, reward) in zip(labels, rewards):
+        plt.plot(reward, label=label)
         # print(reward)
-    plt.xlabel("steps", fontsize=20)
-    plt.ylabel("average reward (over 2000 runs)", fontsize=20)
+    plt.xlabel("Steps", fontsize=20)
+    plt.ylabel("Average Reward (Over 2000 Runs)", fontsize=20)
+    plt.title("UCB vs. Ours", fontsize=20)
     plt.legend(fontsize=20)
-    plt.savefig("project.png")
+    plt.savefig("UCB_vs_Ours.png")
     plt.close()
 
 if __name__ == "__main__":
